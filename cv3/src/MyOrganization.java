d329a290d0e3f90b90331f75bc3bdf74702a98e2
Jakub Kassak <mojspam.jk@gmail.com>
2021-10-08 22:59:23 +0200

    Function generateTree
    Function returns root of binary tree of WorkUnits of size n.

--- cv3/src/MyOrganization.java
+++ cv3/src/MyOrganization.java
@@ -2,9 +2,25 @@ import workbalance.*;
 import java.util.Random;
 
 public class MyOrganization {
+    static Random rnd = new Random();
+    static int counter = 0;
+
+
+    static WorkUnit generateTree(int n, String name, WorkUnit parent){
+        if (n == 1){
+            Worker worker = new Worker("Member" + name + '.' + counter++, rnd.nextInt(10) * 10 + 100);
+            worker.addWorkload(rnd.nextInt(3));
+            return worker;
+        }
+
+        Team team = new Team("Team" + name);
+        team.addMember(generateTree(n - 1, name+".l", team));
+        team.addMember(generateTree(n -1, name+".r", team));
+        return team;
+    }
 
     public static void main(String[] args) {
-        Random random = new Random();
+        /*
         WorkUnit myOrg = new Team("MyOrg");
         for (int i = 1; i < 5; i++) {
             String teamName = "Team_" + i;
@@ -12,18 +28,22 @@ public class MyOrganization {
             for (int j = 10; j <= 30; j += 10) {
                 String memberName = "Member_" + i + "-" + j;
                 WorkUnit member = new Worker(memberName, 10 * i * j);
-                member.addWorkload(random.nextInt(2));
+                member.addWorkload(rnd.nextInt(2));
                 team.addMember(member);
             }
             myOrg.addMember(team);
         }
+        */
 
-        System.out.println(myOrg.repr());
+        WorkUnit myOrg = generateTree(4, "MyOrg", null);
+
+        System.out.println(myOrg.repr() + "\n");
 
         for (int i = 0; i < 4; i++) {
             myOrg.addWorkload(10);
-            System.out.println(myOrg.repr());
+            System.out.println(myOrg.repr()+ "\n");
         }
+
 
     }
 }
