bce375c5092c1713486de411a35a856fb20b3361
Jakub Kassak <mojspam.jk@gmail.com>
2021-10-09 12:24:03 +0200

    class DieTester
    function testConsecutivePairs , function evalEntropy

--- cv3/src/MyRandomDemo.java
+++ cv3/src/MyRandomDemo.java
@@ -1,9 +1,14 @@
 import dnd.D4;
 import dnd.D6;
+import dnd.Dn;
+import dnd.NDie;
+import static dnd.DieTester.testDistribution;
+import static dnd.DieTester.testConsecutivePairs;
 
 public class MyRandomDemo {
 
     public static void main(String[] args) {
+        /*
         D6 d6 = new D6();
 
         for (int i = 0; i < 10; i++) {
@@ -17,5 +22,10 @@ public class MyRandomDemo {
             System.out.print(d4.nextValue() + " ");
         }
         System.out.println();
+        */
+        NDie die = new Dn(4);
+        int tries = 100;
+        testDistribution(die, tries);
+        testConsecutivePairs(die, tries);
     }
 }
--- cv3/src/dnd/DieTester.java
+++ cv3/src/dnd/DieTester.java
@@ -1,16 +1,55 @@
 package dnd;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
+import static java.lang.Math.log;
 
 public class DieTester {
 
-    static void testDistribution(NDie die, int tries) {
+    public static void testDistribution(NDie die, int tries) {
+        int n = die.getSidesCount();
         Map<Integer, Integer> distribution = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            distribution.put(i, 0);
+        }
         for (int i = 0; i < tries; i++) {
             int value = die.nextValue();
-            distribution.put(value, distribution.getOrDefault(value, 0) + 1);
+            distribution.put(value, distribution.get(value) + 1);
         }
         System.out.println(distribution);
+        System.out.println(evalEntropy(distribution.values(), tries));
+    }
+
+
+    public static void testConsecutivePairs(NDie die, int tries) {
+        int n = die.getSidesCount();
+        Map<String, Integer> distribution = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                String key = i + "x" + j;
+                distribution.put(key, 0);
+            }
+        }
+
+        int prevVal = die.nextValue();
+        for (int i = 0; i < tries; i++) {
+            int nextValue = die.nextValue();
+            String key = prevVal + "x" + nextValue;
+            distribution.put(key, distribution.get(key) + 1);
+            prevVal = nextValue;
+        }
+        System.out.println(distribution);
+        System.out.println(evalEntropy(distribution.values(), tries));
+    }
+
+    private static double evalEntropy(Collection<Integer> distribution, int n){
+        double sum = 0;
+        for (Integer value : distribution){
+            if (value != 0) {
+                double p = value / (double) n;
+                sum -= p * log(p) / log(2);
+            }
+        }
+        return sum;
     }
 }
